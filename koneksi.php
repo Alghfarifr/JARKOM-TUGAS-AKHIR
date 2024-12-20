2f6aacca35bc5feee785d6bfcead3687e8ec91ff
Alghfarifr <152803716+Alghfarifr@users.noreply.github.com>
2024-12-20 08:52:31 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,25 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "manajemen_mobil";
+// Koneksi ke database manajemen_user
+$host_user = "localhost";
+$user_user = "root";
+$pass_user = "";
+$db_user   = "manajemen_user";
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn_user = new mysqli($host_user, $user_user, $pass_user, $db_user);
 
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+if ($conn_user->connect_error) {
+    die("Koneksi ke database manajemen_user gagal: " . $conn_user->connect_error);
 }
+
+// Koneksi ke database manajemen_mobil
+// $host_mobil = "localhost";
+// $user_mobil = "root";
+// $pass_mobil = "";
+// $db_mobil   = "manajemen_mobil";
+
+// $conn_mobil = new mysqli($host_mobil, $user_mobil, $pass_mobil, $db_mobil);
+
+// if ($conn_mobil->connect_error) {
+//     die("Koneksi ke database manajemen_mobil gagal: " . $conn_mobil->connect_error);
+// }
 ?>
